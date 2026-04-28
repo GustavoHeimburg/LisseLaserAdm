8b4083eccb439b19862f1eddf66437227e778a89
Gustavoheimburg <gutogustavogu@gmail.com>
2026-04-27 22:09:08 -0300

    Refatoracao

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $servicos = json_decode(file_get_contents("data/servicos.json"), true);
 
     <script src="https://cdn.tailwindcss.com"></script>
 
-    <link rel="icon" type="image/png" href="src/images/favlasr.png">
+    <link rel="icon" type="image/png" href="src/images/favicon.ico">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
           integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
